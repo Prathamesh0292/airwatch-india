bc055ec33f4dc7941f879ac672a7de2dcfc3116b
Prathamesh <your@email.com>
2026-03-14 20:22:14 +0530

    feat: initial project structure

---
+++ src/test/java/com/example/airwatch/AirwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirwatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
